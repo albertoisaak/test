903ff6fb2f66ad49af608d0004df95ad8eb34b5b
Jorge Alberto Isaak Lozano <alberto@Jorges-MacBook-Air.local>
2024-10-24 10:00:59 -0600

    proyecto

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # test
+# Python Plotting Program
+
+Este es un programa en Python que genera un histograma y un gráfico de dispersión utilizando un dataset de muestra. El programa carga un archivo CSV, selecciona las columnas especificadas y visualiza los gráficos.
+
+## Requisitos
+
+- Python 3.x
+- Las siguientes librerías de Python deben estar instaladas:
+
+  - `plotly`
+  - `pandas`
+  - `streamlit` 
--- app.py
+++ app.py
@@ -1,52 +1,27 @@
 import pandas as pd
-import scipy.stats
+import plotly.express as px
 import streamlit as st
-import time
 
-# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
-if 'experiment_no' not in st.session_state:
-    st.session_state['experiment_no'] = 0
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+disp_button = st.button('Construir grafico dispersion') # crear un botón
 
-if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
 
-st.header('Lanzar una moneda')
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
 
-chart = st.line_chart([0.5])
+if disp_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un grafico de dispersion
+    fig = px.scatter(car_data, x="odometer", y="price") 
 
-def toss_coin(n):
-
-    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
-
-    mean = None
-    outcome_no = 0
-    outcome_1_count = 0
-
-    for r in trial_outcomes:
-        outcome_no +=1
-        if r == 1:
-            outcome_1_count += 1
-        mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
-        time.sleep(0.05)
-
-    return mean
-
-number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
-start_button = st.button('Ejecutar')
-
-if start_button:
-    st.write(f'Experimento con {number_of_trials} intentos en curso.')
-    st.session_state['experiment_no'] += 1
-    mean = toss_coin(number_of_trials)
-    st.session_state['df_experiment_results'] = pd.concat([
-        st.session_state['df_experiment_results'],
-        pd.DataFrame(data=[[st.session_state['experiment_no'],
-                            number_of_trials,
-                            mean]],
-                     columns=['no', 'iterations', 'mean'])
-        ],
-        axis=0)
-    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
-
-st.write(st.session_state['df_experiment_results'])
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
