f743e915e627e3ffeab79e5abcff32e84c9dce84
albertoisaak <153254307+albertoisaak@users.noreply.github.com>
2024-10-24 17:13:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.title("Python Plotting Program")
+st.subheader("Este es un programa en Python que genera un histograma y un gráfico de dispersión utilizando un dataset de muestra. El programa carga un archivo CSV, selecciona las columnas especificadas y visualiza los gráficos.")
+
+
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
 disp_button = st.button('Construir grafico dispersion') # crear un botón
 
-st.title("Python Plotting Program")
-st.subheader("Este es un programa en Python que genera un histograma y un gráfico de dispersión utilizando un dataset de muestra. El programa carga un archivo CSV, selecciona las columnas especificadas y visualiza los gráficos.")
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
